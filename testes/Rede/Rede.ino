d45e2a390614b04ddc3adcfcf0c8c8a565177838
ivancley <ivancley@hotmail.com>
2023-06-03 22:59:03 -0300

    Sct013

---
+++ testes/Rede/Rede.ino
@@ -0,0 +1,28 @@
+/*
+ Projeto - teste módulo ENC28J60 
+ Autor: Ivancley Brito 
+ 03/06/23
+ Apenas testes para ver se a config está OK
+ /////////////////////////////////// Pinagem 
+  CS  -> D10
+  SI  -> D11
+  SO  -> D12
+  SCK -> D13
+  RST -> RST
+  INT -> D2
+  VCC -> 3V3 
+  GND -> GND
+*/
+
+#include "etherShield.h"
+#include "ETHER_28J60.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
